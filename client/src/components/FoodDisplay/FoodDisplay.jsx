5986581245eb704b188b6cf4b57e37f3780108e6
Jaikumar <kanishjaikumar06@gmail.com>
2025-12-27 21:38:50 +0530

    Added view more button to the food items

--- client/src/components/FoodDisplay/FoodDisplay.jsx
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -6,17 +6,28 @@ import FoodItem from '../FoodItem/FoodItem'
 const FoodDisplay = ({ category }) => {
 
     const { food_list } = useContext(StoreContext)
+    const [visibleCount, setVisibleCount] = React.useState(4)
+
+    React.useEffect(() => {
+        setVisibleCount(4)
+    }, [category])
+
+    const filteredItems = food_list.filter(item => category === "All" || category === item.category);
+    const displayItems = filteredItems.slice(0, visibleCount);
 
     return (
         <div className='food-display' id='food-display'>
             <h2>Top dishes near you</h2>
             <div className="food-display-list">
-                {food_list.map((item, index) => {
-                    if (category === "All" || category === item.category) {
-                        return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
-                    }
+                {displayItems.map((item, index) => {
+                    return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image} />
                 })}
             </div>
+            {visibleCount < filteredItems.length && (
+                <div className="view-more-container">
+                    <button onClick={() => setVisibleCount(prev => prev + 4)}>View More</button>
+                </div>
+            )}
         </div>
     )
 }
